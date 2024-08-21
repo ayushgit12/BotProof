eb173ba697e383b6d5691ba7e799611eda005c14
Dev Chandra Modak <23je0299@iitism.ac.in>
2024-08-22 00:14:00 +0530

    added css for captcha

--- frontend/src/components/captcha.jsx
+++ frontend/src/components/captcha.jsx
@@ -1,52 +1,43 @@
 import React, { Component } from 'react';
-import { loadCaptchaEnginge, LoadCanvasTemplate, LoadCanvasTemplateNoReload, validateCaptcha } from 'react-simple-captcha';
-
-
+import { loadCaptchaEnginge, LoadCanvasTemplate, validateCaptcha } from 'react-simple-captcha';
 
 class CaptchaTest extends Component {
-
-   componentDidMount () {
+   componentDidMount() {
       loadCaptchaEnginge(6); 
    };
 
    doSubmit = () => {
        let user_captcha = document.getElementById('user_captcha_input').value;
 
-       if (validateCaptcha(user_captcha)===true) {
+       if (validateCaptcha(user_captcha) === true) {
            alert('Captcha Matched');
            loadCaptchaEnginge(6); 
            document.getElementById('user_captcha_input').value = "";
-       }
-
-       else {
+       } else {
            alert('Captcha Does Not Match');
            document.getElementById('user_captcha_input').value = "";
        }
    };
 
    render() {
-        
-
-       return (<div>
-           <div className="container">
-               <div className="form-group">
-
-                   <div className="col mt-3">
-                       <LoadCanvasTemplate />
-                   </div>
-
-                   <div className="col mt-3">
-                       <div><input placeholder="Enter Captcha Value" id="user_captcha_input" name="user_captcha_input" type="text"></input></div>
-                   </div>
-
-                   <div className="col mt-3">
-                       <div><button class="btn btn-primary" onClick={() => this.doSubmit()}>Submit</button></div>
-                   </div>
-                     
-               </div>
-
+       return (
+           <div className="captcha-container">
+               <div className="captcha-header">CAPTCHA</div>
+               <LoadCanvasTemplate />
+               <input 
+                   className="captcha-input"
+                   placeholder="Enter Captcha Value"
+                   id="user_captcha_input"
+                   name="user_captcha_input" 
+                   type="text"
+               />
+               <button 
+                   className="captcha-button"
+                   onClick={this.doSubmit}>
+                   Submit
+               </button>
            </div>
-       </div>);
+       );
    };
 }
 
