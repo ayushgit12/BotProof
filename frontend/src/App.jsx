ed3bc7f2db0305a3972b19f4ade8aa2378d4c650
Dhruv <mradhruv460@gmail.com>
2024-08-21 23:37:55 +0530

    added captcha route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import CaptchaTest from './components/captcha.jsx'
-
+import {BrowserRouter as Router,Routes,Route, BrowserRouter} from 'react-router-dom'
 function App() {
  
 
   return (
 
+<Router>
+  <Routes>
+    <Route path="/captcha" element={<CaptchaTest />} />
+  </Routes>
+</Router>
 
-<CaptchaTest/>
 
   
   );
