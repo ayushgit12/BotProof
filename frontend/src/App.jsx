68c64c28136cdb7ff0035468f5aed96b4790d00c
Dhruv <mradhruv460@gmail.com>
2024-08-23 00:39:10 +0530

    changes to frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-// import './App.css'
+import './App.css'
 import CaptchaTest from './components/captcha.jsx'
 import SignUp from './components/signup.jsx'
 import {BrowserRouter as Router,Routes,Route, BrowserRouter} from 'react-router-dom'
+import DemoPage from './components/DemoPage.jsx'
 function App() {
  
 
@@ -14,6 +15,7 @@ function App() {
   <Routes>
     <Route path="/captcha" element={<CaptchaTest />} />
     <Route path="/signup"  element ={<SignUp/>} />
+      <Route path="/demopage"  element ={<DemoPage/>} />
   </Routes>
 </Router>
 
