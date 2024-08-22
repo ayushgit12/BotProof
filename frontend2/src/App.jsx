f9d10991895ef4b9a41fd86ba9fd8ad3743c2520
Dhruv <mradhruv460@gmail.com>
2024-08-23 04:19:56 +0530

    changes made to landing

--- frontend2/src/App.jsx
+++ frontend2/src/App.jsx
@@ -1,39 +1,54 @@
-import { useState, useEffect } from 'react'
-import { Navigation } from './components/navigation'
-import { Header } from './components/header'
-import { Features } from './components/features'
-import { About } from './components/about'
-import { Services } from './components/services'
-import { Gallery } from './components/gallery'
-import { Testimonials } from './components/testimonials'
-import { Footer } from './components/footer'
-import JsonData from './data/data.json'
-import SmoothScroll from 'smooth-scroll'
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { Navigation } from './components/navigation';
+import { Header } from './components/header';
+import { Features } from './components/features';
+import { About } from './components/about';
+import { Services } from './components/services';
+import { Gallery } from './components/gallery';
+import { Testimonials } from './components/testimonials';
+import { Footer } from './components/footer';
+import JsonData from './data/data.json';
+import SmoothScroll from 'smooth-scroll';
+import React from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import CaptchaTest from './components/Captcha';
 
 export const scroll = new SmoothScroll('a[href*="#"]', {
   speed: 1000,
   speedAsDuration: true,
-})
+});
 
 const App = () => {
-  const [landingPageData, setLandingPageData] = useState({})
+  const [landingPageData, setLandingPageData] = useState({});
+
   useEffect(() => {
-    setLandingPageData(JsonData)
-  }, [])
+    setLandingPageData(JsonData);
+  }, []);
 
   return (
-    <div>
-      <Navigation />
-      <Header data={landingPageData.Header} />
-      <Features data={landingPageData.Features} />
-      <About data={landingPageData.About} />
-      <Services data={landingPageData.Services} />
-      <Gallery />
-      <Testimonials data={landingPageData.Testimonials} />
-      <Footer />
-    </div>
-  )
-}
+    <BrowserRouter>
+      <div>
+        <Navigation />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Header data={landingPageData.Header} />
+                <Features data={landingPageData.Features} />
+                <About data={landingPageData.About} />
+                <Services data={landingPageData.Services} />
+                <Gallery />
+                <Testimonials data={landingPageData.Testimonials} />
+              </>
+            }
+          />
+          <Route path="/captcha-test" element={<CaptchaTest />} />
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
